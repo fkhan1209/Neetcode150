3ac5d6a96cbb1a4993f337f8532446c5e68bcbd9
fkhan1209 <fkhan1209@gmail.com>
2023-06-21 01:10:39 -0400

    Solution for Leetcode Problem 217

--- Solutions/Contains_Duplicate_217.py
+++ Solutions/Contains_Duplicate_217.py
@@ -1,5 +1,5 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def containsDuplicate(self, nums: list[int]) -> bool:
         length = len(nums) #finds the length of the original list of numbers 
         nums_set = set(nums) #instatiates a set of the numbers list -- removing all duplicates
         set_length = len (nums_set) #finds the length of the set of numbers  
